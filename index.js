7821129f03c6d7482a9d526b34af676ca5e75e97
Carly Pecora <carlyannpecora@gmail.com>
2019-04-01 15:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,9 +34,14 @@ function iLoveTheBeatles(num){
   var arr = []
   do{
     arr.push("I love the Beatles!")
+<<<<<<< HEAD
     num++
   } while(num < 15);
   return arr
 }
 
 iLoveTheBeatles(10)
+=======
+  } while(num < 15);
+}
+>>>>>>> 4842352652211cfc9a1ddc8be99ea3ddfc681d54
